612eddcc86de6abcae5d0d02010d46b0e0ea4c0d
gerfaot <soldvds@gmail.com>
2022-10-12 02:55:34 +0300

    Init project with Spring

---
+++ src/test/java/org/leonid/taxadviserapp/TaxadviserappApplicationTests.java
@@ -0,0 +1,13 @@
+package org.leonid.taxadviserapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaxadviserappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
