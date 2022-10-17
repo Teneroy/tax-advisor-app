e78e210cc17cedcad436c2731ac61ca289f7247b
gerfaot <soldvds@gmail.com>
2022-10-17 20:27:38 +0300

    Made services markup

---
+++ src/main/java/org/leonid/taxadviserapp/services/CompanyService.java
@@ -0,0 +1,47 @@
+package org.leonid.taxadviserapp.services;
+
+import org.leonid.taxadviserapp.dao.CompanyDAO;
+import org.leonid.taxadviserapp.entities.Company;
+import org.leonid.taxadviserapp.entities.User;
+
+import java.util.List;
+
+public class CompanyService {
+
+    private final CompanyDAO companyRepository;
+
+    public CompanyService(CompanyDAO companyRepository) {
+        this.companyRepository = companyRepository;
+    }
+
+    public List<Company> getAllCompanies() {
+        return companyRepository.getAllCompanies();
+    }
+
+    public boolean addCompany(Company company) {
+        return companyRepository.addCompany(company);
+    }
+
+    public Company findCompanyById(int id) {
+        return companyRepository.getCompanyById(id);
+    }
+
+    public Company findCompanyByName(String name) {
+        return companyRepository.findCompanyByName(name);
+    }
+
+    public boolean updateCompany(Company company){
+        return companyRepository.updateCompany(company);
+    }
+
+    public List<User> getUsersByCompanyId(int id) {
+
+
+        return companyRepository.getUsersByCompanyId(id);
+    }
+
+    public boolean deleteCompany(Company company) {
+        return companyRepository.deleteCompany(company);
+    }
+
+}
--- src/main/java/org/leonid/taxadviserapp/services/UserService.java
+++ src/main/java/org/leonid/taxadviserapp/services/UserService.java
@@ -1,10 +1,49 @@
 package org.leonid.taxadviserapp.services;
 
+import org.leonid.taxadviserapp.dao.UserDAO;
+import org.leonid.taxadviserapp.entities.User;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class UserService {
-    public String getUserById(int id){
+    private final UserDAO userRepository;
+
+    public UserService(UserDAO userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public String getUserById(int id) {
         return "Leonid" + id;
     }
+
+    public boolean addUser(User user) {
+        return userRepository.addUser(user);
+    }
+
+    public User findById(int id) {
+        return userRepository.getUserById(id);
+    }
+
+    public List<User> getAllUsers() {
+        return userRepository.getAllUsers();
+    }
+
+    public User searchUsersByName(String name) {
+
+
+        return userRepository.findUserByName(name);
+    }
+
+    public boolean updateUser(User user){
+        return userRepository.updateUser(user);
+    }
+
+    public boolean deleteUser(User user) {
+        return userRepository.deleteUser(user);
+    }
+
+
+
 }
